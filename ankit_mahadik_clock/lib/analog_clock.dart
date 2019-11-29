78599b4a1782dc28a47a5fb1b19feeec64602467
Ankit Mahadik <ankit.mahadik.sa@gmail.com>
2019-11-29 15:48:31 +0530

    optimization branch created

--- ankit_mahadik_clock/lib/analog_clock.dart
+++ ankit_mahadik_clock/lib/analog_clock.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:math';
 
 import 'package:ankit_mahadik_clock/circle_progress_second.dart';
 import 'package:ankit_mahadik_clock/date_text_helper.dart';
@@ -7,23 +6,11 @@ import 'package:ankit_mahadik_clock/lines_painter.dart';
 import 'package:ankit_mahadik_clock/second_hand.dart';
 import 'package:ankit_mahadik_clock/time_lines_painter.dart';
 import 'package:ankit_mahadik_clock/utils.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_clock_helper/model.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/semantics.dart';
 import 'package:intl/intl.dart';
-import 'package:vector_math/vector_math_64.dart' show radians;
 
-/// Total distance traveled by a second or a minute hand, each second or minute,
-/// respectively.
-final radiansPerTick = radians(360 / 60);
-
-/// Total distance traveled by an hour hand, each hour, in radians.
-final radiansPerHour = radians(360 / 12);
-
-/// A basic analog clock.
-///
-/// You can do better than this!
 class AnalogClock extends StatefulWidget {
   const AnalogClock(this.model);
 
@@ -41,15 +28,9 @@ class _AnalogClockState extends State<AnalogClock> {
   Color currentColor = Color(0xFF6A1B9A);
   int currentColorIndex = 0;
 
-  double _height = 0.0, _width = 0.0;
-
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration.zero, () {
-      _height = MediaQuery.of(context).size.height;
-      _width = MediaQuery.of(context).size.width;
-    });
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.landscapeLeft,
       DeviceOrientation.landscapeRight,
@@ -220,74 +201,74 @@ class _AnalogClockState extends State<AnalogClock> {
                 BoxShadow(
                     color: Colors.black26.withOpacity(0.04),
                     blurRadius: 10,
-                    offset: Offset(-12, 0),
+                    offset: const Offset(-12, 0),
                     spreadRadius: 2),
                 BoxShadow(
                     color: Colors.black26.withOpacity(0.04),
                     blurRadius: 10,
-                    offset: Offset(12, 0),
+                    offset: const Offset(12, 0),
                     spreadRadius: 5),
               ]),
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: Container(
-                width: 280,
-                height: 280,
-                child: CustomPaint(
-                  painter: LinesPainter(currentColor, DialLineType.clock),
-                  child: Container(
-                    margin: const EdgeInsets.all(23.0),
-                    decoration: BoxDecoration(
-                        color: bgColor,
-                        shape: BoxShape.circle,
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.black26.withOpacity(0.03),
-                              blurRadius: 5,
-                              spreadRadius: 8),
-                        ]),
-                    child: Container(
-                      height: double.infinity,
-                      width: double.infinity,
-                      child: Stack(
-                        children: <Widget>[
-                          _buildTempBorderWidget(),
-                          _buildTempWidget(),
-                          _buildDateBorderWidget(),
-                          _buildDateWidget(),
-                          _buildMeridiemDialWidget(),
-                          Container(
-                            width: 280,
-                            height: 280,
-                            child: CustomPaint(
-                              painter: TimeLinesPainter(
-                                lineType: LineType.hour,
-                                tick: _hoursPercent(),
-                              ),
-                              child: CustomPaint(
-                                painter: TimeLinesPainter(
-                                  lineType: LineType.minute,
-                                  tick: _minutesPercent(),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            width: 280,
-                            height: 280,
-                            child: SecondHand(
-                                currentTick: _secondPercent(),
-                                prevTick: prevTick),
-                          ),
-                        ],
-                      ),
-                    ),
+            child: Container(
+              width: 280,
+              height: 280,
+              child: CustomPaint(
+                painter: LinesPainter(currentColor, DialLineType.clock),
+                child: Container(
+                  margin: const EdgeInsets.all(23.0),
+                  decoration: BoxDecoration(
+                      color: bgColor,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black26.withOpacity(0.03),
+                            blurRadius: 5,
+                            spreadRadius: 8),
+                      ]),
+                  child: Stack(
+                    children: <Widget>[
+                      _buildTempBorderWidget(),
+                      _buildTempWidget(),
+                      _buildDateBorderWidget(),
+                      _buildDateWidget(),
+                      _buildMeridiemDialWidget(),
+                      _buildHourMinuteHandWidget(),
+                      _buildSecondHandWidget(),
+                    ],
                   ),
                 ),
               ),
             ),
           )),
+    );
+  }
+
+  Widget _buildSecondHandWidget() {
+    return Container(
+      width: 280,
+      height: 280,
+      child: SecondHand(currentTick: _secondPercent(), prevTick: prevTick),
+    );
+  }
+
+  Widget _buildHourMinuteHandWidget() {
+    return Container(
+      width: 280,
+      height: 280,
+      child: CustomPaint(
+        painter: TimeLinesPainter(
+          lineType: LineType.hour,
+          tick: _hoursPercent(),
+        ),
+        child: CustomPaint(
+          painter: TimeLinesPainter(
+            lineType: LineType.minute,
+            tick: _minutesPercent(),
+          ),
+        ),
+      ),
     );
   }
 
